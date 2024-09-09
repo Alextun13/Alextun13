da176c07eecdc4551847322ea7f55b2ab67d27ce
chenaoh <al.lucerob@duocuc.cl>
2024-09-09 20:08:12 -0300

    Logica de login, 1 error

--- ThanosAPP/src/app/pages/login/login.page.ts
+++ ThanosAPP/src/app/pages/login/login.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ElementRef, OnInit,ViewChild } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { DbService } from 'src/app/services/db.service';
+import { Login } from 'src/app/models/login';
 
 @Component({
   selector: 'app-login',
@@ -7,19 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  modeloUsuario : String = "";
-  modeloContrasena : String = "";
+  username!: String;
+  password!: String;
+  mensaje : string;
 
-  constructor() { 
-    console.log("Pagina login iniciada")
+  constructor(
+    private toastController : ToastController,
+    private router : Router,
+    private dbService: DbService,
+  ) { 
+    this.mensaje = "Bienvenido"
   }
 
   ngOnInit() {
   }
 
   validarCredenciales() {
-    console.log(this.modeloUsuario);
-    console.log(this.modeloContrasena);
+    console.log("Ejecutando validacion ...")
+    
+    const login = this.dbService.findByUsername(this.username);
 
+    if(login === undefined){
+      this.generateMessage('Usuario no existe', 'danger');
+      return;
+    }
+
+    if (
+      this.username === login.username &&
+      this.password === login.password
+    ) {
+      this.generateMessage('Login correcto', 'success');
+      let extras: NavigationExtras = {
+        state: { user: this.username }
+      }
+      this.router.navigate(['/home'], extras);
+    } else {
+      this.generateMessage('Login fallido', 'danger');
+    }
+  }
+  async generateMessage(message: string, color: string){
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom',
+      color: color
+    });
+    await toast.present();
   }
 }
+
