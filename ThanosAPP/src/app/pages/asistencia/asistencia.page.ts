7df644a0bb186ecc61c01985212768b3c22be54f
chenaoh <al.lucerob@duocuc.cl>
2024-12-01 01:49:40 -0300

    Registro implementado en la APP

--- ThanosAPP/src/app/pages/asistencia/asistencia.page.ts
+++ ThanosAPP/src/app/pages/asistencia/asistencia.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-asistencia',
   templateUrl: './asistencia.page.html',
@@ -7,26 +8,18 @@ import { Router } from '@angular/router';
 })
 export class AsistenciaPage implements OnInit {
   lista_asistencia: any[] = [];
+
   constructor(
     private router: Router,
   ) { }
+
   ngOnInit() {
-    this.generarAsistencias();
+    // La función generarAsistencias fue eliminada
   }
-  generarAsistencias() {
-    const clases = ['PrograBdd', 'PrograAppMovil', 'Hacking', 'Java1', 'MachineLearning'];
-    this.lista_asistencia = clases.map(clase => ({
-      nom_asig: clase,
-      t_clases: 10, // Número total de clases fijo
-      c_presente: Math.floor(Math.random() * 10), // Clases asistidas aleatorias
-      porcentaje: Math.floor(Math.random() * 100) // Porcentaje aleatorio
-    }));
-  }
+
   volverPaginaPrincipal() {
     this.router.navigate(['/home']); 
   }
-
-
 
   RegistroQR() {
     this.router.navigate(['/registro-qr']); 
