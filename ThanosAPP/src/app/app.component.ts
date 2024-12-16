a244b87220a2ba66ffc6a836b370569991961bb9
cataC20 <103144344+cataC20@users.noreply.github.com>
2024-12-15 21:15:51 -0300

    menu cierre de sesion

--- ThanosAPP/src/app/app.component.ts
+++ ThanosAPP/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { MenuController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,37 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private router: Router,
+    private alertController: AlertController,
+    private menuController: MenuController
+  ) {}
+
+  async cerrarSesion() {
+    const alert = await this.alertController.create({
+      header: 'Cerrar Sesión',
+      message: '¿Estás seguro de que quieres cerrar sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }, 
+        {
+          text: 'Salir',
+          handler: () => {
+            this.menuController.close();
+            
+
+            localStorage.removeItem('token');
+            localStorage.removeItem('usuario');
+            
+            
+            this.router.navigate(['/login']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
